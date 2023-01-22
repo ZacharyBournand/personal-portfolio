0ad5dad59c0210792e858cfbddff2d2c6990ca4a
ZacharyBournand <zachary.bournand@ufl.edu>
2023-01-21 19:16:52 -0500

    Site completed with responsive design for horizontal screen viewing

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+/* skills section */
+// Front-end skills
 let frontEndSkills = document.querySelector('.skills-list-front');
 let frontEndTitle = document.querySelector('.front-end');
 let frontEndList = document.querySelectorAll('.front-end-language');
 
+// Back-end skills
 let backEndSkills = document.querySelector('.skills-list-back');
 let backEndTitle = document.querySelector('.back-end');
 let backEndList = document.querySelectorAll('.back-end-language');
 
+// DBMS skills
 let dbmsSkills = document.querySelector('.skills-list-dbms');
 let dbmsTitle = document.querySelector('.dbms');
 let dbmsList = document.querySelectorAll('.dbms-list');
 
+/* Icons in the home section */
 let linkedinIcon = document.querySelector('#linkedin');
 let githubIcon = document.querySelector('#github');
 
+/* projects sections */
+// Project 1 
+let project1 = document.querySelector('.project-1');
+let project2 = document.querySelector('.project-2');
+
+let image1 = document.getElementById('project-omedocs');
+let image2 = document.getElementById('project-movie-search-engine');
+
+let text1 = document.querySelector('.project-text-container-1');
+let text2 = document.querySelector('.project-text-container-2');
 
 // Prevent "Confirm Resubmission Form" notification every time the user reloads the page
 if ( window.history.replaceState ) {
@@ -98,17 +113,40 @@ dbmsSkills.addEventListener('mouseout', (event) => {
 
 
 linkedinIcon.addEventListener('mouseover', (event) => {
-    linkedinIcon.style.cssText = 'width: 2.5rem; height: 2.5rem';
+    linkedinIcon.style.cssText = 'width: 2.5rem; height: 2.5rem; transition: .5s;';
 });
 
 linkedinIcon.addEventListener('mouseout', (event) => {
-    linkedinIcon.style.cssText = 'width: 1.6rem; height: 1.6rem';
+    linkedinIcon.style.cssText = 'width: 1.6rem; height: 1.6rem; transition: .5s;';
 });
 
 
 githubIcon.addEventListener('mouseover', (event) => {
-    githubIcon.style.cssText = 'width: 2.5rem; height: 2.5rem';
+    githubIcon.style.cssText = 'width: 2.5rem; height: 2.5rem; transition: .5s;';
 });
 githubIcon.addEventListener('mouseout', (event) => {
-    githubIcon.style.cssText = 'width: 1.6rem; height: 1.6rem';
+    githubIcon.style.cssText = 'width: 1.6rem; height: 1.6rem; transition: .5s;';
+});
+
+
+project1.addEventListener('mouseover', (event) => {
+    project1.style.cssText = 'border: 0.19rem solid rgb(61, 102, 102); border-radius: 0.5rem;';
+    image1.style.cssText = 'filter: blur(1vh) brightness(25%)';
+    text1.style.cssText = 'visibility: visible; opacity: 1; transition: opacity 1.5s linear;';
+});
+project1.addEventListener('mouseout', (event) => {
+    project1.style.cssText = 'border: 0.3rem solid rgb(56, 80, 80);';
+    image1.style.cssText = 'filter: blur(0) brightness(100%); transition: 1s';
+    text1.style.cssText = 'visibility: hidden; opacity: 0; transition: opacity 0.5s linear;';
+});
+
+project2.addEventListener('mouseover', (event) => {
+    project2.style.cssText = 'border: 0.19rem solid rgb(61, 102, 102); border-radius: 0.5rem;';
+    image2.style.cssText = 'filter: blur(1vh) brightness(90%)';
+    text2.style.cssText = 'visibility: visible; opacity: 1; transition: opacity 1.5s linear;';
+});
+project2.addEventListener('mouseout', (event) => {
+    project2.style.cssText = 'border: 0.19rem solid rgb(74, 87, 87);';
+    image2.style.cssText = 'filter: blur(0) brightness(100%); transition: 1s';
+    text2.style.cssText = 'visibility: hidden; opacity: 0; transition: opacity 0.5s linear;';
 });
