06afc48e658b83f5265db983f83e719fdd9a1e25
ZacharyBournand <zachary.bournand@ufl.edu>
2023-02-06 01:10:46 -0500

    New comments added in the js file

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let dbmsSkills = document.querySelector('.skills-list-dbms');
 let dbmsTitle = document.querySelector('.dbms');
 let dbmsList = document.querySelectorAll('.dbms-list');
 
-/* projects sections */
+/* projects section */
 // Project 1 
 let project1 = document.querySelector('.project-1');
 let project2 = document.querySelector('.project-2');
@@ -35,12 +35,17 @@ let githubProject2 = document.querySelector('.project-2 #github');
  
 let linkProject1 = document.querySelector('.project-link-text-1');
 
+/* contact section */
+// "Send message" button
+let contactButton = document.querySelector('.contact .button');
+
+
 // Prevent "Confirm Resubmission Form" notification every time the user reloads the page
 if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
 }
 
-
+// Handles contact submission
 function SendMail() {
     var params = {
         from_name: document.getElementById('name').value,
@@ -52,6 +57,7 @@ function SendMail() {
     const serviceID = "service_77m7tca";
     const templateID = "template_m4o1c6k";
 
+    // Uses EmailJS
     emailjs.send(serviceID, templateID, params)
     .then(
         function(res) {
@@ -66,6 +72,9 @@ function SendMail() {
     ).catch((err) => console.log(err));
 }
 
+
+// Only for lanscape orientation (width > height)
+// Only allows icons in the home section to change style when the mouse hovers over them, not clicked on
 if (window.matchMedia("(orientation: landscape)").matches) {
     linkedinHomeIcon.addEventListener('click', (event) => {
         linkedinHomeIcon.style.cssText = 'width: 3vw; height: 3vw; transition: .5s';
@@ -90,6 +99,7 @@ if (window.matchMedia("(orientation: landscape)").matches) {
 }
 
 
+// Changes the style of the list of my front-end skills
 frontEndSkills.addEventListener('mouseover', (event) => {
     for (i = 0; i < frontEndList.length; i++) {
         frontEndList[i].style.color = 'white';
@@ -97,7 +107,6 @@ frontEndSkills.addEventListener('mouseover', (event) => {
     frontEndTitle.style.cssText = 'color: rgb(197, 197, 197); text-decoration-color: #cc0000';
     frontEndSkills.style.cssText = 'border: 0.2rem solid darkslategrey';
 });
-
 frontEndSkills.addEventListener('mouseout', (event) => {
     for (i = 0; i < frontEndList.length; i++) {
         frontEndList[i].style.color = 'rgb(197, 197, 197)';
@@ -106,7 +115,7 @@ frontEndSkills.addEventListener('mouseout', (event) => {
     frontEndSkills.style.cssText = 'border: none';
 });
 
-
+// Changes the style of the list of my back-end skills
 backEndSkills.addEventListener('mouseover', (event) => {
     for (i = 0; i < backEndList.length; i++) {
         backEndList[i].style.color = 'white';
@@ -114,7 +123,6 @@ backEndSkills.addEventListener('mouseover', (event) => {
     backEndTitle.style.cssText = 'color: rgb(197, 197, 197); text-decoration-color: #cc0000';
     backEndSkills.style.cssText = 'border: 0.2rem solid darkslategrey';
 });
-
 backEndSkills.addEventListener('mouseout', (event) => {
     for (i = 0; i < backEndList.length; i++) {
         backEndList[i].style.color = 'rgb(197, 197, 197)';
@@ -123,7 +131,7 @@ backEndSkills.addEventListener('mouseout', (event) => {
     backEndSkills.style.cssText = 'border: none';
 });
 
-
+// Changes the style of the list of my database management system skills
 dbmsSkills.addEventListener('mouseover', (event) => {
     for (i = 0; i < dbmsList.length; i++) {
         dbmsList[i].style.color = 'white';
@@ -131,7 +139,6 @@ dbmsSkills.addEventListener('mouseover', (event) => {
     dbmsTitle.style.cssText = 'color: rgb(197, 197, 197); text-decoration-color: #cc0000';
     dbmsSkills.style.cssText = 'border: 0.2rem solid darkslategrey';
 });
-
 dbmsSkills.addEventListener('mouseout', (event) => {
     for (i = 0; i < dbmsList.length; i++) {
         dbmsList[i].style.color = 'rgb(197, 197, 197)';
@@ -141,6 +148,20 @@ dbmsSkills.addEventListener('mouseout', (event) => {
 });
 
 
+// Only allows the contact button to change style when the mouse hovers over it, not clicked on
+contactButton.addEventListener('click', (event) => {
+    contactButton.style.cssText = 'color: #fff; background-color: none';
+});
+contactButton.addEventListener('mouseover', (event) => {
+    contactButton.style.cssText = 'background-color: #131325; color: red';
+});
+contactButton.addEventListener('mouseout', (event) => {
+    contactButton.style.cssText = 'color: #fff; background-color: none';
+});
+
+
+// Only for lanscape orientation (width > height)
+// Only allows icons in the projects section to change style when the mouse hovers over them, not clicked on
 if (window.matchMedia("(orientation: landscape)").matches) {
     githubProject1.addEventListener('click', (event) => {
         githubProject1.style.cssText = 'width: 2.5vw; height: 2.5vw; transition: .5s';
@@ -174,7 +195,10 @@ if (window.matchMedia("(orientation: landscape)").matches) {
 }
 
 
+// Only for lanscape orientation (width > height)
 if (window.matchMedia("(orientation: landscape)").matches) {
+    // Project images change style when the mouse hovers over them or they are clicked on,
+    // which then displays information about each project (depending on the one hovered over/clicked on)
     project1.addEventListener('mouseover', (event) => {
         project1.style.cssText = 'border: 0.19rem solid rgb(61, 102, 102); border-radius: 0.5rem;';
         image1.style.cssText = 'filter: blur(1vh) brightness(25%)';
@@ -195,10 +219,23 @@ if (window.matchMedia("(orientation: landscape)").matches) {
         project2.style.cssText = 'border: 0.19rem solid rgb(74, 87, 87);';
         image2.style.cssText = 'filter: blur(0) brightness(100%); transition: 1s';
         text2.style.cssText = 'visibility: hidden; opacity: 0; transition: opacity 0.5s linear;';
-    });    
+    });
+    
+    // Only allows the LinkedIn hyperlink in the projects section to change style when the mouse hovers over them, not clicked on
+    linkProject1.addEventListener('click', (event) => {
+        linkProject1.style.cssText = 'font-size: 2vw; font-weight: bold; color: rgba(135, 206, 250, 0.733); text-shadow: none; transition: .5s';
+    });
+    linkProject1.addEventListener('mouseover', (event) => {
+        linkProject1.style.cssText = 'font-size: 2.5vw; color: rgba(135, 206, 250, 0.733); text-shadow: black 0.2rem 0.2rem 0.2rem; transition: .5s';
+    });
+    linkProject1.addEventListener('mouseout', (event) => {
+        linkProject1.style.cssText = 'font-size: 2vw; font-weight: bold; color: rgba(135, 206, 250, 0.733); text-shadow: none; transition: .5s';
+    });
 }
 
 
+// Only for portrait orientation (width < height)
+// Change the project images' style when they are clicked on & clicked out
 if (window.matchMedia("(orientation: portrait)").matches) {
     project1.addEventListener('click', (event) => {
         project1.style.cssText = 'border: 0.19rem solid rgb(61, 102, 102); border-radius: 0.5rem;';
